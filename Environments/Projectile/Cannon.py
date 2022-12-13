56cfdd37d21438bbcf94f1c271d7d920189f2871
Asabs18 <aidan.j.sabatini@icloud.com>
2022-12-13 10:58:01 -0500

    Projectile Updates

--- Environments/Projectile/Cannon.py
+++ Environments/Projectile/Cannon.py
@@ -13,6 +13,11 @@ class Cannon:
         self.x = CANNON_OFFSET_X
         self.y = (floor.getRect()[1] - self.height)
 
+        self.floor = floor
+
+        self.angle = angle
+        self.velocity = velocity
+
     #Displays the cannon on the screen
     def draw(self):
         pygame.draw.rect(self.screen, RED, pygame.Rect(self.x, self.y, self.width, self.height))
@@ -20,3 +25,12 @@ class Cannon:
     #Returns a Rect Object that contains the x and y coords as well as the width and height of the floor
     def getRect(self):
         return pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def getVelocity(self):
+        return self.velocity
+
+    def getAngle(self):
+        return self.angle
+
+    def getWidth(self):
+        return self.width
--- Environments/Projectile/Projectile.py
+++ Environments/Projectile/Projectile.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, math
 from Components.assets import *
 
 pygame.init()
@@ -10,8 +10,21 @@ class Projectile:
         self.width = BALL_W
         self.height = BALL_H
 
-        self.x = cannon.getRect()[0]
-        self.y = cannon.getRect()[1]
+        self.cannon = cannon
+
+        self.x = self.cannon.getRect()[0] + self.cannon.getWidth()
+        self.y = self.cannon.getRect()[1]
+
+        self.velocityX, self.velocityY = self.splitVelocityComponents()
+
+    def splitVelocityComponents(self):
+        velX = (self.cannon.getVelocity()*math.cos(math.radians(self.cannon.getAngle())))
+        velY = (self.cannon.getVelocity()*math.sin(math.radians(self.cannon.getAngle())))
+        return velX, velY
+
+    def updatePosition(self):
+        self.x += int(self.velocityX)
+        self.y -= int(self.velocityY)
 
     def draw(self):
         pygame.draw.rect(self.screen, WHITE, pygame.Rect(self.x, self.y, self.width, self.height))
